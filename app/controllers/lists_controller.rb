db0deac2222714db878fe69f8c7bdfc320501eb5
Haley Warson <haleywarson@gmail.com>
2021-05-27 11:32:16 -0600

    CRUD routes working.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,34 @@
 class ListsController < ApplicationController
+
+    before_action :find_list, only: [:show, :update, :destroy]
+
+    def index
+        @lists = List.all 
+        render json: @lists 
+    end
+
+    def show
+        render json: @list 
+    end 
+
+    def create
+        @list = List.create name: params[:name], items: params[:items]
+        render json: @list 
+    end
+
+    def update
+        @list.update name: params[:name], items: params[:items]
+        render json: @list 
+    end
+
+    def destroy
+        @list.destroy 
+    end 
+
+    private 
+
+    def find_list
+        @list = List.find params[:id]
+    end 
+
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,49 @@
 class TripsController < ApplicationController
+
+    before_action :find_trip, only: [:show, :update, :destroy]
+    
+    def index
+        @trips = Trip.all 
+        render json: @trips, include: [:lists]
+    end
+
+    def show
+        render json: @trip, include: [:lists]
+    end 
+
+    def create
+        @trip = Trip.create(trip_params)
+        render json: @trip
+    end
+
+    def update
+        @trip.update(trip_params)
+        render json: @trip  
+    end
+
+    def destroy
+        @trip.destroy 
+    end 
+
+    private 
+
+    def find_trip
+        @trip = Trip.find params[:id]
+    end 
+
+    def trip_params
+        params.require(:trip).permit(
+            :name, 
+            :location, 
+            :image,
+            :start_date,
+            :end_date,
+            :category,
+            :miles,
+            :difficulty,
+            :rating,
+            :notes
+        )
+    end 
+
 end
